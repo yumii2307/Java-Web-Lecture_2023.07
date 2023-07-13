263b1904cc80433a5cd305dd04d6cada17c43d05
Yumi Kim <yumii2307@naver.com>
2023-07-13 10:48:55 +0900

    JSP

--- src/main/java/ch10_Filter/EncodingFilter.java
+++ src/main/java/ch10_Filter/EncodingFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 
-@WebFilter("/ch11/*")
+@WebFilter("/ch10/*")
 public class EncodingFilter extends HttpFilter implements Filter {
 
     public EncodingFilter() {
